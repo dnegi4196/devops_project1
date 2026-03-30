107ddb489bc2b2675f4d981a34db5f622e9356c7
dinesh <dnegi4196@gmail.com>
2026-03-30 12:02:31 +0530

    fix routing issue

--- app/app.js
+++ app/app.js
@@ -3,7 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
